e88684b42d00824fcd2dc8184da5fdb1a7b14925
Cole Sammons <colemsammons@gmail.com>
2021-09-04 11:02:33 -0700

    intial commit modal setup

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'email not valid']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+//get total count of friends
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+//create User model with schema
+const User = model('User'. UserSchema);
+
+module.exports = User;
